55520df8f675ec62df08137557df098f072634b5
daniacorral <daniacorral@arizona.edu>
2025-01-26 21:00:27 +0000

    testing commit

---
+++ exercises/01_hello/testing.py
@@ -0,0 +1,16 @@
+~/workspace/bin/new.py -h
+usage: new.py [-h] [-n NAME] [-e EMAIL] [-p PURPOSE] [-f] program
+
+Create Python argparse program
+
+positional arguments:
+  program               Program name
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -n NAME, --name NAME  Name for docstring (default: Ken Youens-Clark)
+  -e EMAIL, --email EMAIL
+                        Email for docstring (default: kyclark@gmail.com)
+  -p PURPOSE, --purpose PURPOSE
+                        Purpose for docstring (default: Rock the Casbah)
+  -f, --force           Overwrite existing (default: False)
